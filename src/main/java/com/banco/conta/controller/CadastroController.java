85a262e8d0974354c14e49b21ba27245009ae15b
DigoB <braz.rodrigo94@gmail.com>
2020-10-11 16:20:17 -0300

    Criado Envio de CPF

--- src/main/java/com/banco/conta/controller/CadastroController.java
+++ src/main/java/com/banco/conta/controller/CadastroController.java
@@ -1,8 +1,13 @@
 package com.banco.conta.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import com.banco.conta.controller.forms.PcForm;
@@ -21,9 +26,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
-
 
 @RestController
 @RequestMapping("/cadastro")
@@ -32,46 +38,67 @@ public class CadastroController {
     // Responsável por injetar informações externas na Classe atual
     @Autowired
     private PcRepository pcRepository;
-    @Autowired 
+    @Autowired
     private ScRepository scRepository;
     @Autowired
     private EmailDuplicado emailDuplicado;
 
     @InitBinder
-    public void init (WebDataBinder binder){
+    public void init(WebDataBinder binder) {
         binder.addValidators(emailDuplicado);
     }
 
     @PostMapping("/pc")
-    public ResponseEntity <PrimeiroCadastro> primeiroCadastro(@RequestBody @Valid PcForm form, UriComponentsBuilder uri){
+    public ResponseEntity<?> primeiroCadastro(@RequestBody @Valid PcForm form, UriComponentsBuilder uri) {
 
-        PrimeiroCadastro pc = new PrimeiroCadastro(form.getNome(), form.getSobrenome(), form.getEmail(), form.getCnh(), form.getNascimento());
+        PrimeiroCadastro pc = new PrimeiroCadastro(form.getNome(), form.getSobrenome(), form.getEmail(), form.getCnh(),
+                form.getNascimento());
 
         // Se comunica com o banco de dados, salva as informações e gera o id
         pcRepository.save(pc);
 
         URI path = uri.path("/cadastro/sc/{id}").buildAndExpand(pc.getId()).toUri();
-        return ResponseEntity.created(path).body(pc);
+        return ResponseEntity.created(path).build();
 
     }
+
     @PostMapping("/sc/{id}")
-    public ResponseEntity <SegundoCadastro> segundoCadastro(@PathVariable @Valid Long id, @RequestBody @Valid ScForm form, 
-                                                            UriComponentsBuilder uri){
-        
-        // Criando o segundo cadastro e trazendo as informações do primeiro enquanto verifica se as mesmas estão válidas
-        Optional <PrimeiroCadastro> pc = pcRepository.findById(id);
-        if (!pc.isPresent()){
+    public ResponseEntity<?> segundoCadastro(@PathVariable @Valid Long id, @RequestBody @Valid ScForm form,
+            UriComponentsBuilder uri) {
+
+        // Criando o segundo cadastro e trazendo as informações do primeiro enquanto
+        // verifica se as mesmas estão válidas
+        Optional<PrimeiroCadastro> pc = pcRepository.findById(id);
+        if (!pc.isPresent()) {
             throw new IllegalStateException("Cadastro não encontrado");
         }
-        SegundoCadastro sc = new SegundoCadastro(form.getCep(), form.getRua(), form.getBairro(), form.getComplemento(), form.getCidade(), 
-                    form.getEstado(), pc.get());
-
+        SegundoCadastro sc = new SegundoCadastro(form.getCep(), form.getRua(), form.getBairro(), form.getComplemento(),
+                form.getCidade(), form.getEstado(), pc.get());
 
         scRepository.save(sc);
 
         URI path = uri.path("/cadastro/tc/{id}").buildAndExpand(sc.getId()).toUri();
-        return ResponseEntity.created(path).body(sc);
+        return ResponseEntity.created(path).build();
 
+    }
+
+    @PostMapping("/tc/{id}")
+    public ResponseEntity<?> uploadCpf(@PathVariable Long id, @RequestParam @Valid MultipartFile fotoFile,
+            UriComponentsBuilder uriBuilder, HttpServletRequest request) {
+
+        PrimeiroCadastro pcObj = pcRepository.findById(id).orElseThrow(() -> new IllegalStateException("Cliente não encontrado"));
+
+        Path dir = Paths.get("src\\main\\java\\com\\banco\\conta\\resources");
+        String s = dir.toAbsolutePath().toString();
+
+        try {
+            File foto = new File(s + "/" + pcObj.getNome() + id + "." + fotoFile.getOriginalFilename().split("\\.")[1]);
+            fotoFile.transferTo(foto);
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        URI path = uriBuilder.path("/cadastro/qc/{id}").buildAndExpand(id).toUri();
+        return ResponseEntity.created(path).build();
     }
 
 }
